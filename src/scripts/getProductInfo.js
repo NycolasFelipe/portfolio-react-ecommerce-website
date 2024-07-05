684e738dfb4e7cd61f3c86c86a0ee88e2846eb20
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-07-05 18:57:40 -0300

    refactor(getProductInfo): clear code reading

--- src/scripts/getProductInfo.js
+++ src/scripts/getProductInfo.js
@@ -1,27 +1,38 @@
 import getProduct from "../api/getProduct";
 import getProductDetail from "../api/getProductDetail";
 
+/**
+ * This asynchronous function retrieves information about a product identified
+ * by its productId. It utilizes two separate API calls:
+ * - `getProduct(productId)`: Fetches basic product information.
+ * - `getProductDetail(productId)`: Retrieves detailed product information.
+ * 
+ * The retrieved data is then combined and processed before being returned.
+ * 
+ * @param {number} productId The unique identifier of the product to retrieve 
+ * information for 
+ * @returns {Promise<object> | object}
+ * 
+ * @author Nycolas Felipe
+ */
+
 async function getProductInfo(productId) {
-  let productInfo;
+  const id = parseInt(productId);
+  if (typeof (id) !== "number") {
+    console.debug("getProductInfo - error: productId is not a valid integer number");
+    return {};
+  }
   try {
-    const product = await getProduct(productId);
-    const productDetail = await getProductDetail(productId);
-    // Combine data 
-    productInfo = product[0];
-    productInfo.Info = productDetail;
-
-    // Convert JSON data
-    let description = productInfo.Info[0].Description.replace(/\\/g, '');
-    let details = productInfo.Info[0].Details.replace(/\\/g, '');
-    description = JSON.parse(description);
-    details = JSON.parse(details);
-    productInfo.Info[0].Description = description;
-    productInfo.Info[0].Details = details;
-
+    const product = await getProduct(id).then(data => data[0]);
+    const productDetail = await getProductDetail(id);
+    const description = JSON.parse(productDetail[0].Description.replace(/\\/g, ''));
+    const details = JSON.parse(productDetail[0].Details.replace(/\\/g, ''));
+    const stock = JSON.parse(productDetail[0].Stock);
+    product.Info = { Description: description, Details: details, Stock: stock };
+    return product;
   } catch (err) {
-    console.error(err);
-  } finally {
-    return productInfo;
+    console.debug(err);
+    return {};
   }
 }
 
